04837c0455d03ed613802cd632caf5227fd6240b
davidnmora <davidnmora@gmail.com>
2020-05-13 12:05:45 -0700

    ... whoops add color file

---
+++ src/colors.js
@@ -0,0 +1,22 @@
+export const COLORS = {
+  mystic: '#D8E8E8',
+  sinbad: '#A8D0DA',
+  sanMarino: '#406D96',
+  bluewood: '#2F3A56',
+  cinnibar: '#E2474B',
+  sunrise: '#dee45e',
+
+  greenApple: '#13c513',
+}
+
+export const YEAR_COLORS = {
+  2018: COLORS.sinbad,
+  2019: COLORS.bluewood,
+  2020: COLORS.sanMarino,
+}
+
+export const TREND_COLORS = {
+  Boost: COLORS.greenApple,
+  Plummet: COLORS.cinnibar,
+  // Equal: COLORS.bluewood,
+}
