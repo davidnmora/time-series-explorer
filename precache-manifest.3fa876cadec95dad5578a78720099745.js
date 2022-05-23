dc0cd7a1fe290cc6b174e67d3183e92e53bef7d5
David Mora <davidnmora@gmail.com>
2022-05-23 13:28:39 -0700

    Updates

---
+++ precache-manifest.3fa876cadec95dad5578a78720099745.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc9ec37d50dd6a58f71116c671e30e5d",
+    "url": "/time-series-explorer/index.html"
+  },
+  {
+    "revision": "ab20c86778ffb56bc7a8",
+    "url": "/time-series-explorer/static/css/2.df1e88b2.chunk.css"
+  },
+  {
+    "revision": "ab20c86778ffb56bc7a8",
+    "url": "/time-series-explorer/static/js/2.26988ce0.chunk.js"
+  },
+  {
+    "revision": "ebf7e36af5b01c32d80722a72ede6cb6",
+    "url": "/time-series-explorer/static/js/2.26988ce0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27165a730cbeff54cada",
+    "url": "/time-series-explorer/static/js/main.89a6a362.chunk.js"
+  },
+  {
+    "revision": "2eea6382d00f15d98cbb",
+    "url": "/time-series-explorer/static/js/runtime-main.11cf3576.js"
+  },
+  {
+    "revision": "f1a2d150d7ee15e9b0e1bb18801cf82e",
+    "url": "/time-series-explorer/static/media/cori-logo-black.f1a2d150.png"
+  },
+  {
+    "revision": "8f0cf1e8d059aca1a6fd4f75858604bd",
+    "url": "/time-series-explorer/static/media/county-wikipedia-fallback-image.8f0cf1e8.jpg"
+  }
+]);
